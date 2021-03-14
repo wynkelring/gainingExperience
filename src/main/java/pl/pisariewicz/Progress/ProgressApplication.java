1ff69f562d6ac26b018017f42072e6afa06aea0f
solid <solidSRV@gmail.com>
2021-03-14 15:48:28 +0100

    Initial commit

---
+++ src/main/java/pl/pisariewicz/Progress/ProgressApplication.java
@@ -0,0 +1,13 @@
+package pl.pisariewicz.Progress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pisariewicz/Progress/ProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pisariewicz.Progress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
